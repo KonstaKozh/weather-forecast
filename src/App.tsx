c3b7d9dd9a8db1fab9f4a9e755e523a068522381
Konstakozh <konstakozh@yandex.ru>
2021-05-15 00:22:25 +0400

    cra

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navbar} from "./components/Navbar";
+
+const App: React.FC = () => {
+  return <>
+        <h1>weather-forecast</h1>
+        <Navbar />
+      </>
+
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Navbar: React.FunctionComponent = () => {
+    return <div className='nav-wrapper'>Navbar</div>
+}
