752ad6cca657452b20ce4ee7334c5465b4c03f77
KonstaKozh <59201265+KonstaKozh@users.noreply.github.com>
2021-05-17 17:05:34 +0400

    fixed css component WeaterCardItem

--- src/components/ForecastOneDay.tsx
+++ src/components/ForecastOneDay.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import {FormSelect} from "./FormSelect";
-import {WeatherCardList} from "./WeatherCardList";
+import {WeatherCardItem} from "./WeatherCardItem";
 
 export const ForecastOneDay: React.FunctionComponent = () => {
     return <div className='forecast-seven-days-wrapper'>
         <h2>Forecast for a Date in the Past</h2>
         <FormSelect />
-        <WeatherCardList />
+        <WeatherCardItem />
     </div>
 }
