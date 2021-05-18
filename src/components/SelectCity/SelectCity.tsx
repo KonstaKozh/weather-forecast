6af16cecd33171b8b3c787637813918e4e583b71
Konstakozh <konstakozh@yandex.ru>
2021-05-18 23:35:02 +0400

    edit SelectCity.tsx

--- src/components/SelectCity/SelectCity.tsx
+++ src/components/SelectCity/SelectCity.tsx
@@ -1,15 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 import './SelectCity.css'
 
 export const SelectCity: React.FunctionComponent = () => {
+
+    const [addrtype, setAddrtype] = useState(['Самара', 'Тольятти', 'Саратов', 'Казань', 'Краснодар'])
+    const Add = addrtype.map(Add => Add )
+    const handleCitySelect = (e: any) => {
+        console.log(e.target)
+    }
     return (
-        <select className='select'>
-            <option selected className='option'>Select city</option>
-            <option>Самара</option>
-            <option>Тольятти</option>
-            <option>Саратов</option>
-            <option>Казань</option>
-            <option>Краснодар</option>
+        <select className='select' onChange={(e) => handleCitySelect(e)}>
+            <option selected disabled hidden>Select city</option>
+            {
+                Add.map((address, key) => <option key={key}value={key}>{address}</option>)
+            }
         </select>
     )
 }
