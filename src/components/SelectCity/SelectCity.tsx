20abf74e360b0c23669213a645181611a8fa3155
Konstakozh <konstakozh@yandex.ru>
2021-05-19 23:47:42 +0400

    fix SelectCity.tsx

--- src/components/SelectCity/SelectCity.tsx
+++ src/components/SelectCity/SelectCity.tsx
@@ -1,17 +1,42 @@
 import React, {useState} from "react"
 import './SelectCity.css'
+import {cities} from '../../cityData.json'
 
 export const SelectCity: React.FunctionComponent = () => {
 
-    const [city, setСity] = useState(['Самара', 'Тольятти', 'Саратов', 'Казань', 'Краснодар'])
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState({})
+    const keyAPI: string = '3271c2ed7c22a57273a4549fd585d36f'
+
     const handleCitySelect = (e: any) => {
-        console.log(city[e.target.value])
+        const lat: any = cities.find(item => {
+            if (item.name === e.target.value) {
+                return item.coordinates.lat
+            }
+        })
+
+        // const url: string = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={keyAPI}`;
+        // const fetchWeatherData = async (value: string) => {
+        //     setLoading(true)
+        //     const result = await fetch(url)
+        //     setData(result.data)
+        //     setLoading(false)
+        // }
+        // console.log(e.target.value)
     }
+
     return (
         <select className='select' onChange={e => handleCitySelect(e)}>
             {/*<option selected disabled hidden value='1'>Select city</option>*/}
             {
-                city.map((address, key) => <option key={key} value={key}>{address}</option>)
+                // console.log(cities)
+                cities.map((city: any, index: number) => {
+                    // const id = city.id
+                    // const name = city.name
+                    const {id: any, name: string} = city
+                    console.log(id)
+                    return (<option key={id} value={index}>{name}</option>)
+                })
             }
         </select>
     )
