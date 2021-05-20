13611e7395739c8dae0985d5b3a6af0e492873ff
KonstaKozh <59201265+KonstaKozh@users.noreply.github.com>
2021-05-20 12:50:45 +0400

    fix SelectCity

--- src/components/SelectCity/SelectCity.tsx
+++ src/components/SelectCity/SelectCity.tsx
@@ -4,37 +4,33 @@ import {cities} from '../../cityData.json'
 
 export const SelectCity: React.FunctionComponent = () => {
 
-    const [loading, setLoading] = useState(false)
+    // const [loading, setLoading] = useState(false)
     const [data, setData] = useState({})
     const keyAPI: string = '3271c2ed7c22a57273a4549fd585d36f'
 
     const handleCitySelect = (e: any) => {
-        const lat: any = cities.find(item => {
-            if (item.name === e.target.value) {
-                return item.coordinates.lat
-            }
-        })
-
-        // const url: string = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={keyAPI}`;
-        // const fetchWeatherData = async (value: string) => {
-        //     setLoading(true)
-        //     const result = await fetch(url)
-        //     setData(result.data)
-        //     setLoading(false)
-        // }
-        // console.log(e.target.value)
+        const selectedCity = cities[e.target.value]
+        fetchWeatherData(selectedCity)
     }
+
+    const fetchWeatherData = async (city: { name?: string; id?: number; coordinates: any }) => {
+        const {lat, lon} = city.coordinates
+        const url: string = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${keyAPI}`;
+        // setLoading(true)
+        const result = await fetch(url)
+        const data = await result.json()
+        console.log(data.daily)
+        setData(data.daily)
+        // setLoading(false)
+    }
+
 
     return (
         <select className='select' onChange={e => handleCitySelect(e)}>
             {/*<option selected disabled hidden value='1'>Select city</option>*/}
             {
-                // console.log(cities)
-                cities.map((city: any, index: number) => {
-                    // const id = city.id
-                    // const name = city.name
-                    const {id: any, name: string} = city
-                    console.log(id)
+                cities.map((city, index) => {
+                    const {id, name} = city
                     return (<option key={id} value={index}>{name}</option>)
                 })
             }
