485a404392e97919219ba142c2b6fef8eb0ce5ee
KonstaKozh <59201265+KonstaKozh@users.noreply.github.com>
2021-06-16 15:52:19 +0400

    Add files map.js

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,6 +1,21 @@
 // map<T, V>(callback: (element: T) => V, collection: T[]): V[];
 
-const arrayItem = []
-function mapFunctionNew(arrayItem) {arrayItem.reduce(() => {
+const mapArray = [1, 2, 3, 4]
+let i
+const mapCallback = (i) => i + i
+const mapArrayNew = []
+const reducer = (mapArrayNew, currentValue) => {
+    mapCallback(currentValue)
+}
 
-})}
+console.log(mapArray.reduce(reducer))
+
+// const mapFunctionNew = (mapArray, mapCallback) => {
+//     mapArray.reduce((mapArrayNew, currentValue) => {
+//         console.log(mapArrayNew, mapCallback(currentValue))
+//         return mapArrayNew.push(mapCallback(currentValue))
+//     })
+//     // return mapArrayNew
+// }
+
+// console.log(mapFunctionNew(mapArray, mapCallback))
