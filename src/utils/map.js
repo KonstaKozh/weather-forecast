6e83dd42628d203943dbba4aef800ca969fb6508
KonstaKozh <59201265+KonstaKozh@users.noreply.github.com>
2021-06-17 17:34:39 +0400

    Add jsDoc

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,11 +1,23 @@
 // map<T, V>(callback: (element: T) => V, collection: T[]): V[];
 
-const mapArray = [1, 2, 3, 4]
 let i
+/**
+ *  @function mapCallback - The callback that for reduce.
+ *  @param {*} i
+ */
 const mapCallback = (i) => i + i
-const mapArrayNew = []
+
+/**
+ * Function map through reduce.
+ * @function mapFunctionNew
+ * @param {any[]} mapArray
+ * @param {any[]} mapArrayNew
+ */
+
+
 
 const mapFunctionNew = (mapArray, mapCallback) => mapArray.reduce(
     (mapArrayNew, currentValue) => [...mapArrayNew, mapCallback(currentValue)], [])
 
-console.log(mapFunctionNew(mapArray, mapCallback))
+
+console.log(mapFunctionNew([1, 2, 3, 4], i + i))
